b7d31d2b1c4af7d7af31c2e0b7d7da9c9b35af65
David Jensen <Tsourdox@icloud.com>
2020-03-31 09:28:26 +0200

    Updates

---
+++ precache-manifest.dea1311f94d47149499830f3b613abb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d82928932a6e85094bfd60962048d9",
+    "url": "/gh-pages-deploy/index.html"
+  },
+  {
+    "revision": "04e6bca9a5021f89f8b8",
+    "url": "/gh-pages-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a122d99545c0b59a45e3",
+    "url": "/gh-pages-deploy/static/js/2.bc66d670.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-deploy/static/js/2.bc66d670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e6bca9a5021f89f8b8",
+    "url": "/gh-pages-deploy/static/js/main.4989b64f.chunk.js"
+  },
+  {
+    "revision": "ca30b6bb2b8063c0cfd1",
+    "url": "/gh-pages-deploy/static/js/runtime-main.407d2ad3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
